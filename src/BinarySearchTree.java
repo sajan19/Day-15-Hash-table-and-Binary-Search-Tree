338beaa88508908463d5117f8be67fd46e3d8117
Sajan <sajanmhatre29@gmail.com>
2021-09-08 00:05:36 +0530

    Create BST from figure

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -52,6 +52,16 @@ public class BinarySearchTree {
         tree.add(56);
         tree.add(30);
         tree.add(70);
+        tree.add(22);
+        tree.add(40);
+        tree.add(60);
+        tree.add(95);
+        tree.add(11);
+        tree.add(65);
+        tree.add(3);
+        tree.add(16);
+        tree.add(63);
+        tree.add(67);
         tree.inOrder();
     }
 }
